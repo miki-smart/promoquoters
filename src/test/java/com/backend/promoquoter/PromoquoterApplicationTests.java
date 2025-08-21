f25df54488f1e8544d5beca6c7aec5eb26eb1cd5
mikismart <mikiyasshemsu@gmail.com>
2025-08-21 20:54:48 +0300

    intial setup of promoquoter

---
+++ src/test/java/com/backend/promoquoter/PromoquoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.promoquoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoquoterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
