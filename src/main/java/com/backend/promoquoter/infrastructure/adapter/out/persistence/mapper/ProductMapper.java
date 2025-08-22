e7e10840b6229716e1eb4e3322d295fee66dd54f
mikismart <mikiyasshemsu@gmail.com>
2025-08-22 17:39:16 +0300

    => Hexagonal Architecture setup => Necssary files and configurations for Promotions, Product and Order is added => Application, Domain and Infrastructure files are added => Controllers and api endpoints are added with Not Implemented status code response => Application configuration and dependency are added => Dto's, models and entities are added

--- src/main/java/com/backend/promoquoter/infrastructure/adapter/out/persistence/mapper/ProductMapper.java
+++ src/main/java/com/backend/promoquoter/infrastructure/adapter/out/persistence/mapper/ProductMapper.java
@@ -1,34 +1,9 @@
 package com.backend.promoquoter.infrastructure.adapter.out.persistence.mapper;
 import com.backend.promoquoter.infrastructure.adapter.in.web.dto.ProductDtos;
-import com.backend.promoquoter.infrastructure.adapter.out.persistence.dto.ProductDto;
 import com.backend.promoquoter.domain.model.Product;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ProductMapper {
-    // This class will contain methods to map between Product entities and DTOs
-
-     public ProductDtos toDto(Product product) {
-         // Mapping logic here
-         return new ProductDtos(product.getId(), product.getName(), product.getPrice());
-     }
-     public Product toEntity(ProductDtos productDto) {
-         // Mapping logic here
-         return new Product(productDto.getId(), productDto.getName(), productDto.get
-     price());
-     }
-     public List<ProductDtos> toDtoList(List<Product> products) {
-         return products.stream()
-                 .map(this::toDto)
-                 .collect(Collectors.toList());
-     }
-
-     public List<Product> toEntityList(List<ProductDtos> productDtos) {
-         return productDtos.stream()
-                 .map(this::toEntity)
-                 .collect(Collectors.toList());
-     }
-
-
 }
